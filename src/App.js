4840dcf74fb3bdc0fd10d59dcab8af80efcaa7d1
Samprith-Gowda <samprith79@gmail.com>
2024-11-09 12:44:27 +0530

    Initial commit of React app

--- src/App.js
+++ src/App.js
@@ -23,3 +23,17 @@ function App() {
 }
 
 export default App;
+// src/App.js
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App with CI/CD Pipeline</h1>
+      <p>This React app is deployed using Jenkins, Docker, and Kubernetes.</p>
+    </div>
+  );
+}
+
+export default App;
